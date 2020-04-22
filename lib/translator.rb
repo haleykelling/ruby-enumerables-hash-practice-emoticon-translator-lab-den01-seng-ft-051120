e6cbb2ae0368f9d59116c0559e19a543234ed544
Haley Kelling <haley@kellings.com>
2020-04-22 21:07:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
       end
     end
   end
-  if nil
-    puts "Sorry, that emoticon was not found"
-  end
+  p "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
