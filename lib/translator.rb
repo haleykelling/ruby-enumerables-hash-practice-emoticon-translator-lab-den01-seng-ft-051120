b07fcc0a432f18826577bb0e061a69d4d2c93bc9
Haley Kelling <haley@kellings.com>
2020-04-22 20:48:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,13 +5,13 @@ def load_library(file_path)
   # code goes here
   emoticons = YAML.load_file(file_path)
 
-  emoticons.reduce({}) do |emoticons, (name, symbols)|
+  update = emoticons.reduce({}) do |emoticons, (name, symbols)|
     emoticons[name] ||= {}
     emoticons[name][:english] = symbols[0]
     emoticons[name][:japanese] = symbols[1]
     emoticons
   end
-  p emoticons
+  p update
 end
 load_library("./lib/emoticons.yml")
 
